b832e725d62c0b4576864b0eaa0bcad8ad9de4fe
Alex Blackmore <ablackmore113@gmail.com>
2025-07-15 08:39:19 +1000

    push this (w4)

--- 05/q5.c
+++ 05/q5.c
@@ -1,4 +1,7 @@
 #include <stdint.h>
+#include <stdio.h>
+
+uint32_t six_middle_bits(uint32_t input);
 
 int main(void) {
     uint32_t x;
@@ -8,12 +11,28 @@ int main(void) {
 }
 
 uint32_t six_middle_bits(uint32_t input) {
-    uint32_t middle_bits = input & 0b00000000000001111110000000000000;
+    // 0b010111
+    // = 23
+
     // 0b???????? ???????? ???????? ????????
-    // 0b00000000 00000111 11100000 00000000
-    // 0b00000000 00000??? ???00000 00000000
-    uint32_t result = middle_bits >> 13;
-    // 0b00000000 00000??? ???00000 00000000
-    // 0b00000000 00000000 00000000 00??????
-    return result;
+    // goal:           ^^^ ^^^
+    //                 ??? ???
+    //                                ??????
+
+    uint32_t bitmask = 0b00000000000001111110000000000000;
+
+    uint32_t masked_value = input & bitmask;
+
+    //   0b???????? ???????? ???????? ????????
+    // & 0b00000000 00000111 11100000 00000000
+    // = 0b00000000 00000??? ???00000 00000000
+    // masked_value ^
+
+    uint32_t final_value = masked_value >> 13;
+
+    //   0b00000000 00000??? ???00000 00000000
+    //   >> 13
+    //   0b00000000 00000000 00000000 00??????
+
+    return final_value;
 }
--- 05/q8.c
+++ 05/q8.c
@@ -1,32 +1,42 @@
 #include <stdint.h>
+#include <stdio.h>
+
+uint32_t reverse_bits(uint32_t input);
 
 int main(void) {
     uint32_t x;
     printf("gimme a number ");
     scanf("%d", &x);
-    printf("%d\n", reverse_bits(x));
+    printf("%u\n", reverse_bits(x));
 }
 
 uint32_t reverse_bits(uint32_t input) {
     uint32_t output = 0;
-    // each loop iteration is for the ith bit
+
+    // loop over every bit
     for (int i = 0; i < 32; i++) {
-        uint32_t mask = 1u << i;    // 0b00000000 00000000 00010000 00000000
-                                    //                       i^ (12)
-        uint32_t bit = input & mask;// 0b???????? ???????? ???????? ????????
-                                    // 0b00000000 00000000 00010000 00000000
-                                    // 0b00000000 00000000 000?0000 00000000
+        // consider the bit at { 1 << i }
+        uint32_t bit_mask = 1 << i;
 
-        bit = bit >> i;             // 0b00000000 00000000 000?0000 00000000
-                                    // 0b00000000 00000000 00000000 0000000?
+        uint32_t this_bit = input & bit_mask;
+        // input    0b???????? ???????? ???????? ????????
+        // bit_mask 0b00000000 00000000 00000100 00000000 (i == 10)
+        // this_bit 0b00000000 00000000 00000?00 00000000
 
-        if (bit == 1) {
-            // put 1 in this spot in output
-            bit = bit << i;         // 0b00000000 00000000 00010000 00000000
-            output = output | bit;  // 0b00000000 00000000 00000000 00000000
-                                    // 0b00000000 00000000 00010000 00000000
-        } else {
-            // its already 0
+        // if the bit is 1, put a 1 in output at the opposite position
+        if ((this_bit >> i) == 1) {
+            // this_bit >> i = 0b00000000 00000000 00000000 00000001
+            uint32_t write_bit = 1 << (31 - i);
+
+            // write_bit = 0b00000000 00000000 00000100 00000000
+            output = output | write_bit;
+
+            // output =    0b00000000 00000000 000000?? ????????
+            // write_bit = 0b00000000 00000000 00000100 00000000
+            //           | 0b00000000 00000000 000001?? ????????
+
         }
     }
+
+    return output;
 }
